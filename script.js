7c8154a9cfad63cfe6f839edde6ff201af09d3c2
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2022-09-04 06:15:41 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (var i=0; i<10; i++){
+for (var i=1; i<13; i++){
 	document.querySelector('#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div > div.zp_3Lzj1 > div > div.zp_p7Ra4 > div > div > div > div > div.zp_1ybjt > div > div.zp_1x8SC > div > div > div > div:nth-child(1) > div.zp_3Lc6D > button > div > div').click();
 	await new Promise(r => setTimeout(r, 1000));
 	document.querySelector("body > div.apolloio-css-vars-reset.zp-overlay > div > div > div > a:nth-child(1)").click();
@@ -10,7 +10,7 @@ for (var i=0; i<10; i++){
 	document.querySelector("body > div.apolloio-css-vars-reset.zp-overlay > div > div > div > a:nth-child(2)").click();
 	await new Promise(r => setTimeout(r, 1000));
 	document.querySelector("body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_2N-K8 > button").click();
-	await new Promise(r => setTimeout(r, 5000));
+	await new Promise(r => setTimeout(r, 8000));
 	document.querySelector("#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div > div.zp_3Lzj1 > div > div.zp_p7Ra4 > div > div > div > div > div.zp_1ybjt > div > div.zp_1x8SC > div > div > div > div:nth-child(1) > div.zp_1Gu3n > button:nth-child(3) > div").click();
 	await new Promise(r => setTimeout(r, 1000));
 	document.querySelector("body > div.apolloio-css-vars-reset.zp-overlay > div > div > div > a:nth-child(2)").click();
@@ -18,4 +18,5 @@ for (var i=0; i<10; i++){
 	document.querySelector("body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_3wjKN > form > div > div > div > div > div > div:nth-child(2) > label > div").click();
 	document.querySelector("body > div.apolloio-css-vars-reset.zp.zp-modal.zp_1aV2y > div.zp_2gRpu > div > div > div.zp_2N-K8 > button").click();
 	await new Promise(r => setTimeout(r, 5000));
+	console.log('at loop - ',i)
 }
