aa029b2e8e4e34f37da080e351f8f0b40915f2d4
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2022-09-18 00:28:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function wait(time) {
 	});
 }
 
-for (var i = 1; i < 15; i++) {
+const pages = totalLeads(Number(prompt()));
+
+for (var i = 1; i < pages; i++) {
 	document.querySelector(
 		'#provider-mounter > div > div:nth-child(2) > div:nth-child(2) > div > div.zp_1DSCs > div > div.zp_3Lzj1 > div > div.zp_p7Ra4 > div > div > div > div > div.zp_1ybjt > div > div.zp_1x8SC > div > div > div > div:nth-child(1) > div.zp_3Lc6D > button > div > div'
 	).click();
